47cc96a7e7bfb7f228844d22ec8d10b097136809
Furkan Yıldırım <yildirim.frkn@outlook.com>
2022-12-03 17:04:10 +0300

    component ve nuxt guncelleme

---
+++ components/Tutorial.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="bir">
+    <nav class="ust"><h3>
+      Sipariş Takibi  |  Kolay İade  | Yardım</h3></nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
+
+<style>
+.bir {
+  background-color: red;
+}
+
+.ust {
+  align-items: center;
+  float: right;
+}
+</style>
